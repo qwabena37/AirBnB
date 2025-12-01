1aae9e6b9e4e2e3df37f81effa420bcc44af1cbc
James <qjaymce37@gmail.com>
2025-12-01 12:11:36 +0000

    add url path for hubtel payment

--- listings/urls.py
+++ listings/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from .views import HubtelMoMoPaymentView
 
 urlpatterns = [
     path("", views.home, name="home"),                         # homepage
     path("properties/", views.PropertyListCreateView.as_view(), name="property-list"),
     path("properties/<int:pk>/", views.PropertyRetrieveView.as_view(), name="property-detail"),
+       # ... your existing paths
+    path("hubtel-momo/", HubtelMoMoPaymentView.as_view(), name="hubtel-momo"),
 ]
